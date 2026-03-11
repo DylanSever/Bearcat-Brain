164bbec5bb25c7b12d0ea2941b81d1a8db2b4018
Cameron Black <black.cameron125@gmail.com>
2026-03-11 14:28:21 -0400

    Changed main.jsx to hand off to App.jsx which initializes router and controls whether to show Login page or go to router based on token.

--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -17,10 +17,11 @@ export default function Chat() {
   async function sendToBackend(userMessage) {
     const response = await fetch("/bearcat-brain/api/chat", {
       method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        message: userMessage,
-      }),
+      headers: { 
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${token}`
+      },
+      body: JSON.stringify({ message: userMessage }),
     });
     if (!response.ok) {
       const tmp = await response.text();
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,39 +1,11 @@
-// React Router setup for future multi-page support.
-// The app currently is using one main page but routing setup is here so
-// additional pages can be added easily.
-
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
 
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App.jsx";
 
-import Root from "./layouts/Root.jsx";
-import Chat from "./components/Chat.jsx";
-import Settings from "./components/Settings.jsx";
-import About from "./components/About.jsx";
-import NotFoundPage from "./components/NotFoundPage.jsx";
-
-const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <Root />,
-      children: [
-        { index: true, element: <Chat /> },
-        { path: "/settings", element: <Settings /> },
-        { path: "/about", element: <About /> },
-      ],
-    },
-    { path: "*", element: <NotFoundPage /> },
-  ],
-  {
-    basename: '/bearcat-brain'
-  }
-);
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router = {router} />
-  </StrictMode>,
-)
+    <App />
+  </StrictMode>
+);
